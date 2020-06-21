13da99fb44172f234eed22c1de4f8f37a653d7c9
Vayne228 <Prutkin95@mail.ru>
2020-06-21 18:38:17 +0300

    Add site urls

--- greencore/greenapp/views.py
+++ greencore/greenapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.shortcuts import redirect
+import joblib
 
-# Create your views here.
+loaded_model = joblib.load("C:\\Users\\IVAN\\Desktop\\greenatom\\greenatom-test\\greencore\\greenapp\\models\\model.sav")
+
+class Start(View):
+	template = 'greenapp/start.html'
+	raise_exception = True
+	def get(self, request):
+		return render(request, self.template)
+	def post(self, request):
+		rev = request.POST.dict()['review']
+		res = loaded_model.predict([rev])[0]
+		print(res)
+		return render(request, self.template)
