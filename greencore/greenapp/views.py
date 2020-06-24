56abc8a23ba1587e499ebcf09ef47ed985981a7d
Vayne228 <Prutkin95@mail.ru>
2020-06-24 09:21:20 +0300

    predict model load change

--- greencore/greenapp/views.py
+++ greencore/greenapp/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.shortcuts import redirect
+import os.path
 import joblib
 
-loaded_model = joblib.load("C:\\Users\\IVAN\\Desktop\\greenatom\\greenatom-test\\greencore\\greenapp\\models\\model.sav")
+#load trained model
+GDRAT_abs_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'predict_models/model.sav')
+loaded_model = joblib.load(GDRAT_abs_path)
+
 
 class Start(View):
 	template = 'greenapp/start.html'
 	raise_exception = True
+
 	def get(self, request):
+		#unk is unknown type of review
 		return render(request, self.template, context={'unk':1})
+
 	def post(self, request):
 		rev = request.POST.dict()['review']
 		res = loaded_model.predict([rev])[0]
@@ -17,3 +24,5 @@ class Start(View):
 			return render(request, self.template, context={'rev':rev, 'pos':1})
 		else:
 			return render(request, self.template, context={'rev':rev, 'neg':1})
+
+			
